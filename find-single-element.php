e48a4c9e9db5bca833ebad0562fc676fbf0dde05
ashvinihyalij <ashvinihyalij@gmail.com>
2024-05-07 12:15:09 +0530

    Every element appears twice except for one. Find that single one

--- find-single-element.php
+++ find-single-element.php
@@ -1,6 +1,6 @@
 <?php
 // Given a non-empty array of integers nums,
-//every element appears twice except for one. Find that single one.
+// every element appears twice except for one. Find that single one.
 
 function singleNumber($nums) {    
     $result = [];
